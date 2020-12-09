95c880f63d1a897ccda59c4e29f3fa11fbcea830
eligarlo <eligarlo.developer@gmail.com>
2020-12-09 19:56:23 +0200

    Day-9-sound-board

---
+++ Day-9-sound-board/app.js
@@ -0,0 +1,25 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+sounds.forEach((sound) => {
+  const btn = document.createElement('button')
+  btn.classList.add('btn')
+
+  btn.innerText = sound
+
+  btn.addEventListener('click', () => {
+    stopSongs()
+
+    document.querySelector(`#${sound}`).play()
+  })
+
+  document.querySelector('#buttons').appendChild(btn)
+})
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.querySelector(`#${sound}`)
+
+    song.pause()
+    song.currentTime = 0
+  })
+}
